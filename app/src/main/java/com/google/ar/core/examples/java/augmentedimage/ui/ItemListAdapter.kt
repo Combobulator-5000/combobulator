6640728716c6405797f18a412d75f271da763b79
Seth Hinz <sethhinz@me.com>
2022-03-31 13:08:24 -0700

    fork!!!

--- app/src/main/java/com/google/ar/core/examples/java/augmentedimage/ui/ItemListAdapter.kt
+++ app/src/main/java/com/google/ar/core/examples/java/augmentedimage/ui/ItemListAdapter.kt
@@ -9,7 +9,7 @@ import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import com.google.ar.core.examples.java.augmentedimage.classifier.TrackedItem
+import com.google.ar.core.examples.java.augmentedimage.database.TrackedItem
 import kotlinx.android.synthetic.main.row_item.view.*
 
 
